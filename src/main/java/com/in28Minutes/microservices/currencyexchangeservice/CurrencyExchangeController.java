a1c02635e87935b0dc08d0ce6694197f93905c70
saurabh <saurabh.kumar7064@gmail.com>
2022-11-15 15:01:02 -0600

    added spring datajpa to the controller findByFromAndTo will create a query from spring data jpa "from" and "to" are variable in the model class.
    You can access this the repository and controller using http://localhost:8000/currency-exchange/from/USD/to/INR
    
    http://localhost:8000/currency-exchange/from/EUR/to/INR

--- src/main/java/com/in28Minutes/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ src/main/java/com/in28Minutes/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,5 +1,7 @@
 package com.in28Minutes.microservices.currencyexchangeservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,13 +16,26 @@ public class CurrencyExchangeController {
     @Autowired
     private Environment environment;
 
+    private Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
+
+    @Autowired
+    private CurrencyExchangeRepository repository;
+
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
-        CurrencyExchange currencyExchange = new CurrencyExchange(1000L,from,to, BigDecimal.valueOf(50));
+        logger.info("retrieveExchangeValue called with {} to {}", from, to);
+
+        CurrencyExchange currencyExchange
+                = repository.findByFromAndTo(from, to);
+
+        if(currencyExchange ==null) {
+            throw new RuntimeException
+                    ("Unable to Find data for " + from + " to " + to);
+        }
 
         String port = environment.getProperty("local.server.port");
-
         currencyExchange.setEnvironment(port);
+
         return currencyExchange;
     }
 }
