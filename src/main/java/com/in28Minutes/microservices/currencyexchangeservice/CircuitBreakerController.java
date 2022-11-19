293142e5b37abde6a2f2cae977c01f29e0dfc5b5
saurabh <saurabh.kumar7064@gmail.com>
2022-11-18 18:06:57 -0600

    with circuit breaker by firing 100s of request per sec the requests will served for few times and then after some time the circuit breker will not execut the api and will directly return the response from fallback watch -n 0.1 curl http://localhost:8000/sample-api

--- src/main/java/com/in28Minutes/microservices/currencyexchangeservice/CircuitBreakerController.java
+++ src/main/java/com/in28Minutes/microservices/currencyexchangeservice/CircuitBreakerController.java
@@ -20,15 +20,16 @@ public class CircuitBreakerController {
 
     @GetMapping("/sample-api")
     //@Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
-    @CircuitBreaker(name = "default", fallbackMethod = "hardcodedResponse")
-    //@RateLimiter(name="default")
+    //@CircuitBreaker(name = "default", fallbackMethod = "hardcodedResponse")
+   // @RateLimiter(name="default")
     //@Bulkhead(name="sample-api")
     //10s => 10000 calls to the sample api
     public String sampleApi() {
         logger.info("Sample api call received");
-		ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url",
-					String.class);
-		return forEntity.getBody();
+//		ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url",
+//					String.class);
+//		return forEntity.getBody();
+		return "samplie-api";
      }
 
     public String hardcodedResponse(Exception ex) {
